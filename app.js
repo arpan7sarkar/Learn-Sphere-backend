2afd680bb879500ec446bc44f2eefc512f2f8156
arpan7sarkar <arpansakar@gmail.com>
2025-09-11 04:22:32 +0530

    maked it serverless for deploying

--- app.js
+++ app.js
@@ -10,18 +10,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//connecting with the db and starting server
+// connectDB().then(() => {
+//   const port = process.env.PORT || 5001;
+//   console.log("MongoDb setup done");
+//   app.listen(port, () => {
+//     console.log(
+//       `server has been succesfullly listening at http://localhost:${port}/`
+//     );
+//   });
+// });
+
+let isConnected = false;
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+app.use((req,res,next)=>{
+  if(!isConnected){
+    connectToMongoDB();
+  }
+  next();
+})
+
+
 app.get("/", (req, res) => {
   res.send("Hello from Learn Sphere backend!");
 });
 app.use("/api", router);
 
-//connecting with the db and starting server
-connectDB().then(() => {
-  const port = process.env.PORT || 5001;
-  console.log("MongoDb setup done");
-  app.listen(port, () => {
-    console.log(
-      `server has been succesfullly listening at http://localhost:${port}/`
-    );
-  });
-});
+module.exports = app;
